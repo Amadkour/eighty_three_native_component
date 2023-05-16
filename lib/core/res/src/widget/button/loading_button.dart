9ddf7d7c67f1c04c4881f3d47c9cfe5ddcdf5a02
hussein hamed <husseinhamed811@gmail.com>
2023-05-16 14:16:29 +0300

    update

--- lib/core/res/src/widget/button/loading_button.dart
+++ lib/core/res/src/widget/button/loading_button.dart
@@ -1,4 +1,5 @@
 import 'package:eighty_three_native_component/core/res/theme/colors.dart';
+import 'package:eighty_three_native_component/core/utils/extenstions.dart';
 import 'package:eighty_three_native_component/eighty_three_component.dart';
 import 'package:flutter/material.dart';
 
@@ -34,17 +35,17 @@ class StaggerAnimation extends StatelessWidget {
     this.borderRadius,
     this.hasBottomSaveArea = true,
   })  : buttonSqueezeAnimation = Tween<double>(
-          begin: MediaQuery.of(context).size.width,
-          end: 50.0,
-        ).animate(
-          CurvedAnimation(
-            parent: buttonController,
-            curve: const Interval(
-              0.0,
-              0.150,
-            ),
-          ),
-        ),
+    begin: MediaQuery.of(context).size.width,
+    end: 50.0,
+  ).animate(
+    CurvedAnimation(
+      parent: buttonController,
+      curve: const Interval(
+        0.0,
+        0.150,
+      ),
+    ),
+  ),
         containerCircleAnimation = EdgeInsetsTween(
           begin: const EdgeInsets.only(bottom: 30.0),
           end: EdgeInsets.zero,
@@ -75,24 +76,24 @@ class StaggerAnimation extends StatelessWidget {
         decoration: BoxDecoration(
           border: Border.all(color: borderColor ?? Colors.transparent),
           color: enable
-              ? (backgroundColor ?? AppColors.blackColor)
+              ? (backgroundColor ?? context.theme.backgroundColor)
               : (disableColor ??
-                  (backgroundColor ?? AppColors.blackColor).withOpacity(0.12)),
+              (backgroundColor ?? AppColors.blackColor).withOpacity(0.12)),
           borderRadius: BorderRadius.all(Radius.circular(borderRadius ?? 7.0)),
         ),
         child: buttonSqueezeAnimation.value > 75.0
             ? Text(
-                tr(titleButton!),
-                style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                    color: fontColor ?? Colors.white,
-                    fontWeight: fontWeight ?? FontWeight.w500,
-                    fontFamily: 'SemiBold'),
-              )
+          tr(titleButton!),
+          style: Theme.of(context).textTheme.subtitle1!.copyWith(
+              color: fontColor ?? Colors.white,
+              fontWeight: fontWeight ?? FontWeight.w500,
+              fontFamily: 'SemiBold'),
+        )
             : CircularProgressIndicator(
-                strokeWidth: 1.0,
-                valueColor:
-                    AlwaysStoppedAnimation<Color>(loaderColor ?? Colors.white),
-              ),
+          strokeWidth: 1.0,
+          valueColor:
+          AlwaysStoppedAnimation<Color>(loaderColor ?? Colors.white),
+        ),
       ),
     );
   }
@@ -109,20 +110,20 @@ class StaggerAnimation extends StatelessWidget {
 class LoadingButton extends StatefulWidget {
   const LoadingButton(
       {super.key,
-      this.onTap,
-      required this.isLoading,
-      this.title,
-      this.topPadding = 40,
-      this.backgroundColor,
-      this.fontColor,
-      this.disableColor,
-      this.loaderColor,
-      this.borderColor,
-      this.enable = true,
-      this.fontWeight,
-      this.height = 50,
-      this.hasBottomSaveArea = true,
-      this.borderRadius});
+        this.onTap,
+        required this.isLoading,
+        this.title,
+        this.topPadding = 40,
+        this.backgroundColor,
+        this.fontColor,
+        this.disableColor,
+        this.loaderColor,
+        this.borderColor,
+        this.enable = true,
+        this.fontWeight,
+        this.height = 50,
+        this.hasBottomSaveArea = true,
+        this.borderRadius});
 
   final VoidCallback? onTap;
   final String? title;
@@ -174,32 +175,31 @@ class _LoadingButtonState extends State<LoadingButton>
     return SafeArea(
       top: false,
       bottom: widget.hasBottomSaveArea,
-      child: SingleChildScrollView(
-        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-          SizedBox(height: widget.topPadding),
-          Column(
-            children: <Widget>[
-              StaggerAnimation(
-                key: const Key('loginSubmitButton'),
-                context: context,
-                height: widget.height,
-                fontColor: widget.fontColor,
-                loaderColor: widget.loaderColor,
-                fontWeight: widget.fontWeight,
-                enable: widget.enable,
-                backgroundColor: widget.backgroundColor,
-                disableColor: widget.disableColor,
-                borderColor: widget.borderColor,
-                titleButton: widget.title ?? "LOGIN",
-                borderRadius: widget.borderRadius,
-                buttonController:
-                    _loginButtonController.view as AnimationController,
-                onTap: widget.onTap ?? () {},
-              ),
-            ],
-          ),
-        ]),
-      ),
+      child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+        SizedBox(height: widget.topPadding),
+        Column(
+          children: <Widget>[
+            StaggerAnimation(
+              key: const Key('loginSubmitButton'),
+              context: context,
+              height: widget.height,
+              fontColor: widget.fontColor,
+              loaderColor: widget.loaderColor,
+              fontWeight: widget.fontWeight,
+              enable: widget.enable,
+              backgroundColor:  widget
+                  .backgroundColor,
+              disableColor: widget.disableColor,
+              borderColor: widget.borderColor,
+              titleButton: widget.title ?? "LOGIN",
+              borderRadius: widget.borderRadius,
+              buttonController:
+              _loginButtonController.view as AnimationController,
+              onTap: widget.onTap ?? () {},
+            ),
+          ],
+        ),
+      ]),
     );
   }
 }
