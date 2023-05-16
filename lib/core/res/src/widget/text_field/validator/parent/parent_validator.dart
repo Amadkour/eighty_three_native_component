43846255e4964e8176f4d3799e91696ba08849c0
hussein hamed <husseinhamed811@gmail.com>
2023-05-16 14:56:34 +0300

    update

--- lib/core/res/src/widget/text_field/validator/parent/parent_validator.dart
+++ lib/core/res/src/widget/text_field/validator/parent/parent_validator.dart
@@ -1,4 +1,5 @@
 import 'package:eighty_three_native_component/eighty_three_component.dart';
+import 'package:queen_validators/queen_validators.dart';
 
 abstract class ParentValidator {
   String? Function(String?)? getValidation() {
@@ -9,9 +10,21 @@ abstract class ParentValidator {
     return null;
   }
 
+  @override
   String? Function(String?)? getValidationWithLength(
       {int? minLength, int? maxLength}) {
-    return null;
+    return qValidator(
+      <TextValidationRule>[
+        IsRequired(
+          errorMessage('id_empty'),
+        ),
+        MinLength(
+          minLength ?? 10,
+          errorMessage('id_at_least'),
+        ),
+        MaxLength(maxLength ?? 10, errorMessage('id_greater'))
+      ],
+    );
   }
 
   String? errorMessage(String key, {String? maxLength}) {
