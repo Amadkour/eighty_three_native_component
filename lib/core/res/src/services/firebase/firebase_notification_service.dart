de43f6d38e6986f306757e8b4f70ad069b3bd971
Mohamed Ali <mohamedali@res.inc>
2023-05-22 15:37:02 +0300

    add-firebase-services[22-5]

---
+++ lib/core/res/src/services/firebase/firebase_notification_service.dart
@@ -0,0 +1,69 @@
+import 'dart:developer';
+import 'package:eighty_three_native_component/core/res/src/configuration/top_level_configuration.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class FirebaseNotificationService {
+  final FlutterLocalNotificationsPlugin _localNotificationsPlugin;
+
+  FirebaseNotificationService(this._localNotificationsPlugin);
+
+  Future<void> initNotificationService() async {
+    await FirebaseMessaging.instance.requestPermission(
+      announcement: true,
+      carPlay: true,
+      criticalAlert: true,
+      provisional: true,
+    );
+
+    getDeviceToken().then((String value) {
+      if (type == 'test') {
+        log('device_token:$value');
+      }
+    });
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      await sendLocalMessage(message, message.data['payload'] as String?);
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage event) {
+      final String? json = event.data['payload'] as String?;
+      _mapPayload(json);
+    });
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: AndroidInitializationSettings(
+        "app_logo",
+      ),
+      iOS: DarwinInitializationSettings(),
+    );
+    await _localNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (NotificationResponse details) {},
+    );
+  }
+
+  void _mapPayload(String? payload) {}
+
+  Future<void> sendLocalMessage(
+      RemoteMessage? notification, String? payload) async {
+    await _localNotificationsPlugin.show(
+      notification.hashCode,
+      notification?.notification?.title ??
+          notification?.data['title'] as String?,
+      notification?.notification?.body ?? notification?.data['body'] as String?,
+      const NotificationDetails(
+        android: AndroidNotificationDetails(
+          "com.Res.pay.sa.res_pay_merchant",
+          "Res Pay Merchant",
+          icon: 'app_logo',
+        ),
+      ),
+      payload: payload,
+    );
+  }
+
+  Future<String> getDeviceToken() async {
+    return (await FirebaseMessaging.instance.getToken()) ?? "";
+  }
+}
---
+++ lib/core/res/src/services/firebase/init_firebase.dart
@@ -0,0 +1,14 @@
+import 'package:eighty_three_native_component/core/res/src/services/firebase/Firebase_notification_service.dart';
+import 'package:eighty_three_native_component/core/res/src/services/firebase/firebase_crashlytics_service.dart';
+
+class InitFirebase {
+  final FirebaseCrashlyticsService _firebaseCrashlyticsService;
+  final FirebaseNotificationService _firebaseNotificationService;
+  InitFirebase(
+      this._firebaseCrashlyticsService, this._firebaseNotificationService);
+  Future<void> init() async {
+    // crashlytics
+    await _firebaseCrashlyticsService.init();
+    await _firebaseNotificationService.initNotificationService();
+  }
+}
