42162a8dfa0edf5fa24ad2e7d0e95ad445784a2f
Amira Gomaa <amira.gomaa@res.inc>
2023-05-23 00:49:06 +0300

    show keyboard in bottomsheet

--- lib/core/res/src/widget/bottom_sheet/base_bottom_sheet.dart
+++ lib/core/res/src/widget/bottom_sheet/base_bottom_sheet.dart
@@ -34,102 +34,103 @@ void showCustomBottomSheet({
     isScrollControlled: true,
     backgroundColor: Colors.transparent,
     builder: (BuildContext context) {
-      return Directionality(
-        textDirection: currentUserPermission.isArabic
-            ? TextDirection.rtl
-            : TextDirection.ltr,
-        child: Container(
-          constraints: BoxConstraints(maxHeight: context.height * 0.9),
-          // margin: margin ?? const EdgeInsets.only(top: 60),
-          padding: padding ??
-              const EdgeInsets.only(
-                left: 20,
-                right: 20,
-                top: 25,
-              ),
-          decoration: BoxDecoration(
-            color: color ?? Colors.white,
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(32),
-              topRight: Radius.circular(32),
-            ),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(
-                height: padding == null ? 0 : 25,
-              ),
-              Center(
-                child: MyImage.svgAssets(
-                  url: 'assets/images/moreBottomsheet/Rectangle.svg',
-                  width: 64,
-                  height: 5,
+      return GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Directionality(
+          textDirection: currentUserPermission.isArabic ? TextDirection.rtl : TextDirection.ltr,
+          child: Container(
+            constraints: BoxConstraints(maxHeight: context.height * 0.9),
+            // margin: margin ?? const EdgeInsets.only(top: 60),
+            padding: padding ??
+                const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 25,
                 ),
+            decoration: BoxDecoration(
+              color: color ?? Colors.white,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(32),
+                topRight: Radius.circular(32),
               ),
-              title != ""
-                  ? Padding(
-                      padding: EdgeInsets.only(
-                        top: 30,
-                        left: padding == null ? 0 : 20,
-                        right: padding == null ? 0 : 20,
-                      ),
-                      child: Text(
-                        tr(title),
-                        style: paragraphStyle.copyWith(
-                          color: AppColors.blackColor,
-                          fontSize: 16,
-                        ),
-                      ),
-                    )
-                  : const SizedBox(),
-              Flexible(
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      body,
-                      if (hasButtons) ...<Widget>[
-                        LoadingButton(
-                          hasBottomSaveArea: false,
-                          key: const Key("history_period_apply_button"),
-                          isLoading: false,
-                          title: blackButtonTitle ?? tr('apply'),
-                          onTap: () {
-                            onPressed?.call();
-                            if (autoClose) {
-                              CustomNavigator.instance.pop();
-                            }
-                          },
-                        ),
-                        if (isTwoButtons)
-                          Center(
-                            child: TextButton(
-                              key: const Key("history_period_cancel_button"),
-                              onPressed: () {
-                                CustomNavigator.instance.pop();
-                                onCanceled?.call();
-                              },
-                              child: Text(
-                                whiteButtonText ?? tr('cancel'),
-                                style: buttonsStyle.copyWith(
-                                  fontSize: whiteButtonFontSize,
-                                  color: whiteButtonTextColor ?? Colors.black,
-                                ),
-                              ),
-                            ),
-                          )
-                      ],
-                    ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: padding == null ? 0 : 25,
+                ),
+                Center(
+                  child: MyImage.svgAssets(
+                    url: 'assets/images/moreBottomsheet/Rectangle.svg',
+                    width: 64,
+                    height: 5,
                   ),
                 ),
-              ),
-               SizedBox(
-                height:MediaQuery.of(context).viewInsets.bottom+ 10,
-              )
-            ],
+                title != ""
+                    ? Padding(
+                        padding: EdgeInsets.only(
+                          top: 30,
+                          left: padding == null ? 0 : 20,
+                          right: padding == null ? 0 : 20,
+                        ),
+                        child: Text(
+                          tr(title),
+                          style: paragraphStyle.copyWith(
+                            color: AppColors.blackColor,
+                            fontSize: 16,
+                          ),
+                        ),
+                      )
+                    : const SizedBox(),
+                Flexible(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        body,
+                        if (hasButtons) ...<Widget>[
+                          LoadingButton(
+                            hasBottomSaveArea: false,
+                            key: const Key("history_period_apply_button"),
+                            isLoading: false,
+                            title: blackButtonTitle ?? tr('apply'),
+                            onTap: () {
+                              onPressed?.call();
+                              if (autoClose) {
+                                CustomNavigator.instance.pop();
+                              }
+                            },
+                          ),
+                          if (isTwoButtons)
+                            Center(
+                              child: TextButton(
+                                key: const Key("history_period_cancel_button"),
+                                onPressed: () {
+                                  CustomNavigator.instance.pop();
+                                  onCanceled?.call();
+                                },
+                                child: Text(
+                                  whiteButtonText ?? tr('cancel'),
+                                  style: buttonsStyle.copyWith(
+                                    fontSize: whiteButtonFontSize,
+                                    color: whiteButtonTextColor ?? Colors.black,
+                                  ),
+                                ),
+                              ),
+                            )
+                        ],
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).viewInsets.bottom + 10,
+                )
+              ],
+            ),
           ),
         ),
       );
