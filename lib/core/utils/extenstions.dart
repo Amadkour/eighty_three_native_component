241cbb48e0cfb38c7e2c0b2fe23fcbb21a3f9031
Mohamed Ali <mohamedali@res.inc>
2023-05-28 13:13:04 +0300

    update[28-5]

--- lib/core/utils/extenstions.dart
+++ lib/core/utils/extenstions.dart
@@ -15,6 +15,18 @@ extension Cleaining on TextEditingController {
     }
   }
 }
+extension SaudiExtension on String {
+  String get saudi {
+    String value = this;
+    if (value.startsWith('966')) {
+      value = value.substring(3);
+    } else if (value.startsWith('+966')) {
+      value = value.substring(4);
+    }
+
+    return value;
+  }
+}
 
 // rename it to size extension because it make conflict wit ui size class
 
