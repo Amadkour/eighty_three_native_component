23d99bcb177415918910a3b57cbb23bc61c08377
Mohamed Ali <mohamedali@res.inc>
2023-06-04 13:00:49 +0300

    add-toMap-to-user-permission

--- lib/core/res/src/permissions/permission.dart
+++ lib/core/res/src/permissions/permission.dart
@@ -92,4 +92,20 @@ class UserPermission extends ParentModel {
       return converter.convertToString(key: "country");
     }
   }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": userId,
+      "token": token,
+      "name": name,
+      "username": username,
+      "phone": phone,
+      "country": country,
+      "identity_id": identityId,
+      "email": email,
+      "locale": locale,
+      "image": image,
+      "is_completed": isCompleted,
+    };
+  }
 }
