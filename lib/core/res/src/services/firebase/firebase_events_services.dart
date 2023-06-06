6ecb4f8ff1f4c4e6458ccfd74afc20fa5a6fc532
Ahmed Madkour <amira.gomaa@res.inc>
2023-06-06 12:06:26 +0300

    firebase events refactor

--- lib/core/res/src/services/firebase/firebase_events_services.dart
+++ lib/core/res/src/services/firebase/firebase_events_services.dart
@@ -20,38 +20,18 @@ class FirebaseEvents {
     _firebaseAnalyticsService.logEvent("login_event", parameters: parameters);
   }
 
-  void logRealStateInvestment({required int? realEstateId, required String? amount}) {
-    _firebaseAnalyticsService.logEvent("real_state_invest_event", parameters: <String, dynamic>{
-      "realEstateId": realEstateId ?? 0,
-      "amount": amount ?? "",
-    });
+  void logRealStateInvestment({required Map<String, dynamic> parameters}) {
+    _firebaseAnalyticsService.logEvent("real_state_invest_event", parameters:parameters);
   }
 
   void logUpgradeAccountEvent({required Map<String, dynamic> parameters}) {
-    final List<String> codes = <String>[];
-    final List<MultipartFile> files = <MultipartFile>[];
 
-    // for (final SingleProfessionalInvestor i in attachments) {
-    //   codes.add(i.code!);
-    //   files.add(MultipartFile(i.file!.openRead(), i.file!.lengthSync(),
-    //       filename: i.file!.path.split('/').last));
-    // }
-    _firebaseAnalyticsService.logEvent("add_new_bank_account_event", parameters:
-    <String, dynamic>{
-      "user": currentUserPermission.toMap(),
-      'codes[]': codes,
-      'attachments[]': files
-    });
+
+    _firebaseAnalyticsService.logEvent("add_new_bank_account_event", parameters:parameters);
   }
 
-  void logRequestLoan({
-    required String propertyId,
-  }) {
-    _firebaseAnalyticsService.logEvent("create_loan_event", parameters: <String, dynamic>{
-      'property_id': propertyId,
-      'name': currentUserPermission.name,
-      'user_id': currentUserPermission.identityId
-    });
+  void logRequestLoan({required Map<String, dynamic> parameters}) {
+    _firebaseAnalyticsService.logEvent("create_loan_event", parameters: parameters);
   }
 
   void logCancelLoan() {
@@ -71,141 +51,38 @@ class FirebaseEvents {
   }
 
   void logChargeCreditCardEvent(
-      {required String cardName,
-        required String cardNumber,
-        required String paymentMethodId,
-        required String cvv,
-        required String expireYear,
-        required String expireMonth,
-        required String cardAmount,
-        required int saveCredit}) {
-    _firebaseAnalyticsService.logEvent("charge_credit_card_event", parameters: <String, dynamic>{
-      'holder_name': cardName,
-      'card_number': cardNumber,
-      'payment_method_id': paymentMethodId,
-      'expiry[year]': expireYear,
-      'expiry[month]': expireMonth,
-      'save_credit': saveCredit,
-      'cvv': cvv,
-      'amount': cardAmount,
-    });
+      {required Map<String, dynamic> parameters}) {
+    _firebaseAnalyticsService.logEvent("charge_credit_card_event", parameters: parameters);
   }
 
-  void logAddAmountCreditCardEvent({
-    required int cardId,
-    required String amount,
-    required String cardName,
-  }) {
+  void logAddAmountCreditCardEvent({required Map<String, dynamic> parameters}) {
     _firebaseAnalyticsService
-        .logEvent("add_amount_to_credit_card_event", parameters: <String, dynamic>{
-      'card_id': cardId,
-      'amount': amount,
-      'holder_name': cardName,
-    });
+        .logEvent("add_amount_to_credit_card_event", parameters: parameters);
   }
 
-  void logAddNewCreditCardEvent({
-    required String cardName,
-    required String cardNumber,
-    required String paymentMethodId,
-    required String expireYear,
-    required String cvv,
-    required String expireMonth,
-  }) {
-    _firebaseAnalyticsService.logEvent("add_new_credit_card_event", parameters: <String, dynamic>{
-      'holder_name': cardName,
-      'card_number': cardNumber,
-      'payment_method_id': paymentMethodId,
-      'cvv': cvv,
-      'expiry[year]': expireYear,
-      'expiry[month]': expireMonth,
-    });
+  void logAddNewCreditCardEvent({required Map<String, dynamic> parameters}) {
+    _firebaseAnalyticsService.logEvent("add_new_credit_card_event", parameters: parameters);
   }
 
-  void logDeleteCreditCardEvent({required int cardId}) {
-    _firebaseAnalyticsService.logEvent("delete_credit_card_event", parameters: <String, dynamic>{
-      'card_id': cardId,
-    });
+  void logDeleteCreditCardEvent({required Map<String, dynamic> parameters}) {
+    _firebaseAnalyticsService.logEvent("delete_credit_card_event", parameters:parameters);
   }
 
-  void logWithdrawalAmountFromWalletEvent({
-    String? amount,
-    int? bankAccountId,
-    String? confirmationCode,
-  }) {
+  void logWithdrawalAmountFromWalletEvent({required Map<String, dynamic> parameters}) {
     _firebaseAnalyticsService
-        .logEvent("withdrawal_amount_from_wallet_event", parameters: <String, dynamic>{
-      'bank_account_id': bankAccountId,
-      'amount': amount,
-      'confirmation_code': confirmationCode,
-    });
+        .logEvent("withdrawal_amount_from_wallet_event", parameters:parameters);
   }
 
-  void logAddNewBankAccountEvent({
-    String? accountNumber,
-    int? bankId,
-    String? holderName,
-    String? iban,
-    String? code,
-  }) {
-    _firebaseAnalyticsService.logEvent("add_new_bank_account_event", parameters: <String, dynamic>{
-      'account_number': accountNumber,
-      'bank_id': bankId,
-      'holder_name': holderName,
-      'iban': iban,
-      'confirmation_code': code,
-      'country_id': 1
-    });
+  void logAddNewBankAccountEvent({required Map<String, dynamic> parameters}) {
+    _firebaseAnalyticsService.logEvent("add_new_bank_account_event", parameters: parameters);
   }
 
   void logEditBankAccountEvent(
-      {required String accountNumber,
-        required int bankId,
-        required String holderName,
-        required String iban,
-        required int id,
-        required String code}) {
-    _firebaseAnalyticsService.logEvent("edit_bank_account_event", parameters: <String, dynamic>{
-      'country_id': 1,
-      'account_number': accountNumber,
-      'bank_id': bankId,
-      'holder_name': holderName,
-      'iban': iban,
-      'bank_account_id': id,
-    });
+      {required Map<String, dynamic> parameters}) {
+    _firebaseAnalyticsService.logEvent("edit_bank_account_event", parameters: parameters);
   }
 
-  Future<void> logEditProfileEvent({
-    String? phone,
-    String? email,
-    String? countryId,
-    String? nationalID,
-    String? name,
-    File? image,
-    File? staying,
-    File? passport,
-  }) async {
-    _firebaseAnalyticsService.logEvent("edit_profile_event", parameters: <String, dynamic>{
-      'phone_number': phone,
-      'full_name': name,
-      'email': email,
-      'identity_id': nationalID,
-      'country_id': countryId,
-      if (image!.path != '')
-        'image': await MultipartFile.fromFile(
-          image.path,
-          filename: image.path.split('/').last,
-        ),
-      if (passport!.path != '')
-        'passport': await MultipartFile.fromFile(
-          passport.path,
-          filename: passport.path.split('/').last,
-        ),
-      if (staying!.path != '')
-        'stay': await MultipartFile.fromFile(
-          staying.path,
-          filename: staying.path.split('/').last,
-        )
-    });
+  Future<void> logEditProfileEvent({required Map<String, dynamic> parameters}) async {
+    _firebaseAnalyticsService.logEvent("edit_profile_event", parameters: parameters);
   }
 }
