65bb50973c12933df6c22274832305c1fd3e9b9c
Mohamed Ali <mohamedali@res.inc>
2023-06-13 15:41:15 +0300

    edit

--- lib/core/utils/certificate_handle.dart
+++ lib/core/utils/certificate_handle.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
+import 'package:eighty_three_native_component/core/res/src/configuration/top_level_configuration.dart';
 import 'package:flutter/services.dart';
 
 // Future<SecurityContext> get globalContext async {
@@ -22,11 +23,18 @@ import 'package:flutter/services.dart';
 Future<void> handleSSL(Dio dio) async {
   ByteData clientCertificate =
       await rootBundle.load("assets/certificates/res-app.pem");
-
+  ByteData mockaCertificate =
+      await rootBundle.load("assets/certificates/res-mocka.pem");
+print('we are in ${userOldServer ? 'Mocka' : 'Alibaba'}')
   dio.httpClientAdapter = IOHttpClientAdapter()
     ..onHttpClientCreate = (_) {
       final SecurityContext context = SecurityContext(withTrustedRoots: false);
-      context.useCertificateChainBytes(clientCertificate.buffer.asUint8List());
+      if (userOldServer) {
+        context.useCertificateChainBytes(mockaCertificate.buffer.asUint8List());
+      } else {
+        context
+            .useCertificateChainBytes(clientCertificate.buffer.asUint8List());
+      }
       HttpClient httpClient = HttpClient(context: context);
       httpClient.badCertificateCallback =
           (X509Certificate cert, String host, int port) => true;
