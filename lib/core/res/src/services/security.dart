8e9750570c751265edb3f8be276a5b01b1ceedb0
Mohamed Ali <mohamedali@res.inc>
2023-06-13 12:47:53 +0300

    edit

--- lib/core/res/src/services/security.dart
+++ lib/core/res/src/services/security.dart
@@ -2,14 +2,15 @@ import 'dart:convert';
 
 import 'package:crypto/crypto.dart';
 import 'package:encrypt/encrypt.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 String getHashedCode(String plainText) {
   var bytes1 = utf8.encode(plainText);
   return sha256.convert(bytes1).toString();
 }
 
-Encrypted encryption(String plainText, String newKey) {
-  final key = Key.fromUtf8(newKey);
+Encrypted encryption(String plainText) {
+  final key = Key.fromUtf8(dotenv.env['ENCRYPT_KEY'] ?? "");
   final iv = IV.fromLength(16);
 
   final encrypter = Encrypter(AES(key));
@@ -19,8 +20,8 @@ Encrypted encryption(String plainText, String newKey) {
   return encrypted;
 }
 
-String descryption(String encryptionText, String newKey) {
-  final key = Key.fromUtf8(newKey);
+String descryption(String encryptionText) {
+  final key = Key.fromUtf8(dotenv.env['ENCRYPT_KEY'] ?? "");
   final iv = IV.fromLength(16);
 
   final encrypter = Encrypter(AES(key));
