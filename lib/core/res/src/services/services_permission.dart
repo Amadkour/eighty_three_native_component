a6e24e91f6dceb29b8b5f0d3ca6d0c959f96527b
husseinhamed <husseinhamed@res.inc>
2023-06-19 19:40:14 +0300

    update

--- lib/core/res/src/services/services_permission.dart
+++ lib/core/res/src/services/services_permission.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:app_settings/app_settings.dart';
 import 'package:eighty_three_native_component/core/res/src/constant/shared_orefrences_keys.dart';
+import 'package:eighty_three_native_component/core/res/src/cubit/global_cubit.dart';
 import 'package:eighty_three_native_component/core/res/src/services/navigation.dart';
 import 'package:eighty_three_native_component/core/res/src/widget/button/loading_button.dart';
 import 'package:eighty_three_native_component/eighty_three_component.dart';
@@ -16,7 +17,7 @@ class ServicesPermissions {
       required ImageSource source,
       required String title,
       required String subTitle}) async {
-    PermissionStatus status = await permission.request();
+    PermissionStatus status = await permission.status;
     if (status.isGranted) {
       return await ImagePicker().pickImage(
         source: source,
@@ -24,33 +25,46 @@ class ServicesPermissions {
         maxWidth: 1000,
       );
     } else if (status.isDenied || status.isPermanentlyDenied) {
-      showDialog(
-        context: globalKey.currentContext!,
-        builder: (BuildContext context) => AlertDialog(
-          title: Text(title),
-          content: Text(
-            subTitle,
-          ),
-          actions: <Widget>[
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              width: 150,
-              height: 50,
-              child: LoadingButton(
-                hasBottomSaveArea: false,
-                topPadding: 0,
-                onTap: () async {
-                  CustomNavigator.instance.pop();
-                  await AppSettings.openAppSettings();
-                  await openAppSettings();
-                },
-                title: tr("open_settings"),
-                isLoading: false,
+      final PermissionStatus permissionStatus = await permission.request();
+      if(permissionStatus==PermissionStatus.granted){
+        return await ImagePicker().pickImage(
+          source: source,
+          maxHeight: 1000,
+          maxWidth: 1000,
+        );
+      }
+      else{
+        showDialog(
+          context: globalKey.currentContext!,
+          builder: (BuildContext context) => Directionality(
+            textDirection:isArabic ? TextDirection.rtl:TextDirection.ltr,
+            child: AlertDialog(
+              title: Text(title),
+              content: Text(
+                subTitle,
               ),
-            )
-          ],
-        ),
-      );
+              actions: <Widget>[
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  width: 150,
+                  height: 50,
+                  child: LoadingButton(
+                    hasBottomSaveArea: false,
+                    topPadding: 0,
+                    onTap: () async {
+                      CustomNavigator.instance.pop();
+                      await AppSettings.openAppSettings();
+                      await openAppSettings();
+                    },
+                    title: tr("open_settings"),
+                    isLoading: false,
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      }
     }
     return null;
   }
