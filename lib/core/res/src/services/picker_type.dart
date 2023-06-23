331400b634b146bcec894713cae3f4c8a55f5806
husseinhamed <husseinhamed@res.inc>
2023-06-23 18:04:16 +0300

    update

--- lib/core/res/src/services/picker_type.dart
+++ lib/core/res/src/services/picker_type.dart
@@ -1,4 +1,7 @@
 // import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:eighty_three_native_component/core/res/src/configuration/top_level_configuration.dart';
 import 'package:eighty_three_native_component/core/res/src/constant/shared_orefrences_keys.dart';
 import 'package:eighty_three_native_component/core/res/src/services/navigation.dart';
@@ -17,127 +20,127 @@ class PickerType {
     await showModalBottomSheet(
         context: globalKey.currentContext!,
         builder: (BuildContext context) => SingleChildScrollView(
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(15),
-                      topLeft: Radius.circular(15)),
-                ),
-                height: 120,
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(15),
+                  topLeft: Radius.circular(15)),
+            ),
+            height: 120,
+            child: Padding(
+              padding:
+              const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
                     children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          CircleAvatar(
-                            radius: 30,
-                            backgroundColor:
-                                context.theme.primaryColor.withOpacity(0.1),
-                            child: IconButton(
-                              key: const Key("camera_icon_button_key"),
-                              onPressed: () {
-                                type = 0;
-                                CustomNavigator.instance.pop();
-                              },
-                              icon: Icon(
-                                Icons.photo_camera_rounded,
-                                color: context.theme.primaryColor,
-                                size: 25,
-                              ),
-                            ),
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor:
+                        context.theme.primaryColor.withOpacity(0.1),
+                        child: IconButton(
+                          key: const Key("camera_icon_button_key"),
+                          onPressed: () {
+                            type = 0;
+                            CustomNavigator.instance.pop();
+                          },
+                          icon: Icon(
+                            Icons.photo_camera_rounded,
+                            color: context.theme.primaryColor,
+                            size: 25,
                           ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Center(
-                            child: Text(
-                              tr("camera"),
-                              style: TextStyle(
-                                color: context.theme.primaryColor,
-                                fontFamily: 'Light',
-                                fontSize: 14,
-                              ),
-                            ),
-                          )
-                        ],
+                        ),
                       ),
-                      Column(
-                        children: <Widget>[
-                          CircleAvatar(
-                            radius: 30,
-                            backgroundColor:
-                                context.theme.primaryColor.withOpacity(0.1),
-                            child: IconButton(
-                              key: const Key("user_image_gallery_option_key"),
-                              onPressed: () {
-                                type = 1;
-                                CustomNavigator.instance.pop();
-                              },
-                              icon: Icon(
-                                Icons.photo,
-                                color: context.theme.primaryColor,
-                                size: 25,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Center(
-                            child: Text(
-                              tr("gallery"),
-                              style: TextStyle(
-                                color: context.theme.primaryColor,
-                                fontFamily: 'Light',
-                                fontSize: 14,
-                              ),
-                            ),
-                          )
-                        ],
+                      const SizedBox(
+                        height: 10,
                       ),
-                      // if (isFolder)
-                      //   Column(
-                      //     children: <Widget>[
-                      //       CircleAvatar(
-                      //         radius: 30,
-                      //         backgroundColor:  context.theme.primaryColor.withOpacity(
-                      //             0.1),
-                      //         child: IconButton(
-                      //           onPressed: () {
-                      //             type = 2;
-                      //             CustomNavigator.instance.pop();
-                      //           },
-                      //           icon: Icon(
-                      //             Icons.folder_sharp,
-                      //             size: 25,
-                      //             color:  context.theme.primaryColor,
-                      //           ),
-                      //         ),
-                      //       ),
-                      //       const SizedBox(
-                      //         height: 10,
-                      //       ),
-                      //       Center(
-                      //         child: Text(
-                      //           tr("folder"),
-                      //           style: TextStyle(
-                      //             color:  context.theme.primaryColor,
-                      //             fontFamily: 'Light',
-                      //             fontSize: 14,
-                      //           ),
-                      //         ),
-                      //       )
-                      //     ],
-                      //   ),
+                      Center(
+                        child: Text(
+                          tr("camera"),
+                          style: TextStyle(
+                            color: context.theme.primaryColor,
+                            fontFamily: 'Light',
+                            fontSize: 14,
+                          ),
+                        ),
+                      )
                     ],
                   ),
-                ),
+                  Column(
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor:
+                        context.theme.primaryColor.withOpacity(0.1),
+                        child: IconButton(
+                          key: const Key("user_image_gallery_option_key"),
+                          onPressed: () {
+                            type = 1;
+                            CustomNavigator.instance.pop();
+                          },
+                          icon: Icon(
+                            Icons.photo,
+                            color: context.theme.primaryColor,
+                            size: 25,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Center(
+                        child: Text(
+                          tr("gallery"),
+                          style: TextStyle(
+                            color: context.theme.primaryColor,
+                            fontFamily: 'Light',
+                            fontSize: 14,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  // if (isFolder)
+                  //   Column(
+                  //     children: <Widget>[
+                  //       CircleAvatar(
+                  //         radius: 30,
+                  //         backgroundColor:  context.theme.primaryColor.withOpacity(
+                  //             0.1),
+                  //         child: IconButton(
+                  //           onPressed: () {
+                  //             type = 2;
+                  //             CustomNavigator.instance.pop();
+                  //           },
+                  //           icon: Icon(
+                  //             Icons.folder_sharp,
+                  //             size: 25,
+                  //             color:  context.theme.primaryColor,
+                  //           ),
+                  //         ),
+                  //       ),
+                  //       const SizedBox(
+                  //         height: 10,
+                  //       ),
+                  //       Center(
+                  //         child: Text(
+                  //           tr("folder"),
+                  //           style: TextStyle(
+                  //             color:  context.theme.primaryColor,
+                  //             fontFamily: 'Light',
+                  //             fontSize: 14,
+                  //           ),
+                  //         ),
+                  //       )
+                  //     ],
+                  //   ),
+                ],
               ),
             ),
+          ),
+        ),
         barrierColor: Colors.black.withOpacity(0.3));
 
     if (type != -1) {
@@ -158,17 +161,31 @@ class PickerType {
         ServicesPermissions servicesPermissions = ServicesPermissions();
         if(type == 0){
           result = await servicesPermissions.cameraAndGalleryRequestPermission(
-            isAlreadyOpened: cameraPermissionIsAlreadyOpened,
-            subTitle: tr("camera_description"),
-            permission: Permission.camera,
-            source: ImageSource.camera
+              isAlreadyOpened: cameraPermissionIsAlreadyOpened,
+              subTitle: tr("camera_description"),
+              permission: Permission.camera,
+              source: ImageSource.camera
           );
         }
         else{
+          Permission? permission;
+          if(Platform.isIOS){
+            permission = Permission.photos;
+          }
+          else{
+            final deviceInfoPlugin = DeviceInfoPlugin();
+            final deviceInfo = await deviceInfoPlugin.deviceInfo;
+            if(deviceInfo.data["version"]["sdkInt"]>32){
+              permission = Permission.photos;
+            }
+            else {
+              permission = Permission.storage;
+            }
+          }
           result = await servicesPermissions.cameraAndGalleryRequestPermission(
               isAlreadyOpened: galleryPermissionIsAlreadyOpened,
               subTitle: tr("gallery_description"),
-              permission: Permission.photos,
+              permission: permission,
               source: ImageSource.gallery
           );
         }
