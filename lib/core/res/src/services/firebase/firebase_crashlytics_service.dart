fc0522b5b64263ea90869ef5939e11eea895764a
Mohamed Ali <mohamedali@res.inc>
2023-06-25 18:33:36 +0300

    edit

--- lib/core/res/src/services/firebase/firebase_crashlytics_service.dart
+++ lib/core/res/src/services/firebase/firebase_crashlytics_service.dart
@@ -3,42 +3,34 @@ import 'package:flutter/foundation.dart';
 
 class FirebaseCrashlyticsService {
   void init() {
-    FlutterError.onError = (FlutterErrorDetails details) {
-      // Check if the exception is related to a specific error message (prevent upload keyboard issue)
-      if (details
-          .exceptionAsString()
-          .contains('HardwareKeyboard.handleKeyEvent')) {
-        // Skip logging this exception
-        return;
-      }
-
-      // await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
-      // Pass all uncaught errors from the framework to Crashlytics.
-      // FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-      PlatformDispatcher.instance.onError =
-          (Object exception, StackTrace stackTrace) {
+    // await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+    // Pass all uncaught errors from the framework to Crashlytics.
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+    PlatformDispatcher.instance.onError =
+        (Object exception, StackTrace stackTrace) {
+      if (!exception.toString().contains("hardware_keyboard")) {
         FirebaseCrashlytics.instance
             .recordError(exception, stackTrace, fatal: true);
-        return true;
-      };
-
-      // Isolate.current.addErrorListener(RawReceivePort((List<dynamic> pair) async {
-      //   final List<dynamic> errorAndStacktrace = pair;
-      //   await FirebaseCrashlytics.instance.recordError(
-      //     errorAndStacktrace.first,
-      //     errorAndStacktrace.last is StackTrace
-      //         ? errorAndStacktrace.last as StackTrace
-      //         : null,
-      //   );
-      // }).sendPort);
+      }
+      return true;
     };
 
-    void crash() {
-      FirebaseCrashlytics.instance.crash();
-    }
+    // Isolate.current.addErrorListener(RawReceivePort((List<dynamic> pair) async {
+    //   final List<dynamic> errorAndStacktrace = pair;
+    //   await FirebaseCrashlytics.instance.recordError(
+    //     errorAndStacktrace.first,
+    //     errorAndStacktrace.last is StackTrace
+    //         ? errorAndStacktrace.last as StackTrace
+    //         : null,
+    //   );
+    // }).sendPort);
+  }
 
-    Future<void> recordError(String error) async {
-      await FirebaseCrashlytics.instance.recordError(error, StackTrace.empty);
-    }
+  void crash() {
+    FirebaseCrashlytics.instance.crash();
+  }
+
+  Future<void> recordError(String error) async {
+    await FirebaseCrashlytics.instance.recordError(error, StackTrace.empty);
   }
 }
