55b3716ef8b912e179b2c6a0af39e2cd47e4b2ac
Mohamed Ali <mohamedali@res.inc>
2023-07-03 11:39:13 +0300

    edit

--- lib/core/res/src/services/firebase/firebase_crashlytics_service.dart
+++ lib/core/res/src/services/firebase/firebase_crashlytics_service.dart
@@ -3,17 +3,19 @@ import 'package:flutter/foundation.dart';
 
 class FirebaseCrashlyticsService {
   void init() {
-    // await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
-    // Pass all uncaught errors from the framework to Crashlytics.
-    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-    PlatformDispatcher.instance.onError =
-        (Object exception, StackTrace stackTrace) {
-      if (!exception.toString().contains("hardware_keyboard")) {
-        FirebaseCrashlytics.instance
-            .recordError(exception, stackTrace, fatal: true);
-      }
-      return true;
-    };
+    if (kReleaseMode) {
+      // await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+      // Pass all uncaught errors from the framework to Crashlytics.
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+      PlatformDispatcher.instance.onError =
+          (Object exception, StackTrace stackTrace) {
+        if (!exception.toString().contains("hardware_keyboard")) {
+          FirebaseCrashlytics.instance
+              .recordError(exception, stackTrace, fatal: true);
+        }
+        return true;
+      };
+    }
 
     // Isolate.current.addErrorListener(RawReceivePort((List<dynamic> pair) async {
     //   final List<dynamic> errorAndStacktrace = pair;
