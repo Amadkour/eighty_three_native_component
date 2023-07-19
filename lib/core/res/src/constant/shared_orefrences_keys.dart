d37f1175d628901c28e213f3d7295f4c6491a948
Amira gomaa <amira.gomaa@res.inc>
2023-07-19 18:09:37 +0300

    refactor list(ssl from remote config and refactor api connection)

--- lib/core/res/src/constant/shared_orefrences_keys.dart
+++ lib/core/res/src/constant/shared_orefrences_keys.dart
@@ -1,23 +1,4 @@
 import 'package:flutter/cupertino.dart';
 
 final GlobalKey<NavigatorState> globalKey = GlobalKey<NavigatorState>();
-// String userToken = 'token';
-// String userName = 'name';
-// String fullName = 'full_name';
-// String userCountry = 'country';
-// String serverKey = 'server';
-// String cameraPermission = 'camera_permission';
-// String galleryPermission = 'gallery_permission';
-//
-// String userCurrency = 'currency';
-// String userId = 'identity_id';
-// String userDate = 'dob';
-// String userPinCode = 'pin_code';
-// String userTouchId = 'touch_id_active';
-// String userFaceId = 'face_id_active';
-// String userUUID = 'uuid';
-// String userPhone = 'phone_number';
-// String userEmail = 'email';
-// String appInstalled = 'install';
-// String role = 'role';
-// String profileCompleted = 'is_completed';
+
